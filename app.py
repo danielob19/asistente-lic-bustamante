31dc4831fdc26037d1278cf736ca52844c87b3cb
danielob19 <dobar19@yahoo.com>
2024-11-27 23:51:43 -0300

    Update app.py ACTUALIZACION PUERTO

--- app.py
+++ app.py
@@ -64,6 +64,7 @@ def asistente():
 
     return jsonify({"respuesta": respuesta_openai})
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+import os
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
