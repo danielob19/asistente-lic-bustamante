c912610e7d25aa2c993c430879d5276a99269ebd
danielob19 <dobar19@yahoo.com>
2024-12-19 22:09:21 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 import time
 import threading
 import sqlite3
+import openai
 from fastapi import FastAPI, HTTPException, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse, HTMLResponse
