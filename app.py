ddb4c6f4b4ff0129fe432667a57a4fdaf567f335
danielob19 <dobar19@yahoo.com>
2025-02-01 17:05:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -229,7 +229,7 @@ def registrar_interaccion(user_id: str, consulta: str):
 palabras_irrelevantes = {
     "un", "una", "el", "la", "lo", "es", "son", "estoy", "siento", "me siento", "tambien", "tambien tengo", "que", "de", "en", 
     "por", "a", "me", "mi", "tengo", "mucho", "muy", "un", "poco", "tengo", "animicos", "si", "supuesto", "frecuentes", "verdad", "sé", "hoy", "quiero", 
-    "bastante", "mucho", "tambien", "gente", "frecuencia", "entendi", "hola", "estoy", "vos", "entiendo", 
+    "bastante", "mucho", "tambien", "gente", "frecuencia", "entendi", "estoy", "vos", "entiendo", 
     "soy", "mi", "de", "es", "4782-6465", "me", "siento", "para", "mucha", "y", "sufro", "vida", 
     "que", "opinas", "¿","?", "reinicia", "con", "del", "necesito", "me", "das"
 }
