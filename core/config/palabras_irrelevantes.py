60fe1ffa224634ff1d541e57c43ff43b5c6358ce
danielob19 <dobar19@yahoo.com>
2025-05-19 20:17:11 -0300

    Create palabras_irrelevantes.py

---
+++ core/config/palabras_irrelevantes.py
@@ -0,0 +1,7 @@
+palabras_irrelevantes = {
+    "un", "una", "el", "la", "lo", "es", "son", "estoy", "siento", "me siento", "tambien", "tambien tengo", "que", "de", "en",
+    "por", "a", "me", "mi", "tengo", "mucho", "muy", "un", "poco", "tengo", "animicos", "si", "supuesto", "frecuentes", "verdad", "sé", "hoy", "quiero",
+    "bastante", "mucho", "tambien", "gente", "frecuencia", "entendi", "hola", "estoy", "vos", "entiendo",
+    "soy", "mi", "de", "es", "4782-6465", "me", "siento", "para", "mucha", "y", "sufro", "vida",
+    "que", "opinas", "¿", "?", "reinicia", "con", "del", "necesito", "me", "das"
+}
