ec62f6dcfc9f73495227f1a1988e53b34a2542bc
danielob19 <dobar19@yahoo.com>
2025-05-19 20:11:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -225,15 +225,6 @@ def init_db():
     except Exception as e:
         print(f"Error al inicializar la base de datos: {e}")
 
-# Lista de palabras irrelevantes
-palabras_irrelevantes = {
-    "un", "una", "el", "la", "lo", "es", "son", "estoy", "siento", "me siento", "tambien", "tambien tengo", "que", "de", "en", 
-    "por", "a", "me", "mi", "tengo", "mucho", "muy", "un", "poco", "tengo", "animicos", "si", "supuesto", "frecuentes", "verdad", "sé", "hoy", "quiero", 
-    "bastante", "mucho", "tambien", "gente", "frecuencia", "entendi", "hola", "estoy", "vos", "entiendo", 
-    "soy", "mi", "de", "es", "4782-6465", "me", "siento", "para", "mucha", "y", "sufro", "vida", 
-    "que", "opinas", "¿","?", "reinicia", "con", "del", "necesito", "me", "das"
-}
-
 
 # Clase para solicitudes del usuario
 class UserInput(BaseModel):
