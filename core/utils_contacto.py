22444bcd547257e42ea5220dbbc2ca148640c957
danielob19 <dobar19@yahoo.com>
2025-05-18 21:54:59 -0300

    Create utils_contacto.py

---
+++ core/utils_contacto.py
@@ -0,0 +1,5 @@
+def obtener_mensaje_contacto():
+    return (
+        "En caso de que desees contactar al Lic. Daniel O. Bustamante, "
+        "podés hacerlo escribiéndole al WhatsApp +54 911 3310-1186, que con gusto responderá a tus inquietudes."
+    )
