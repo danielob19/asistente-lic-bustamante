5bfc8c19fd664edfbe071a50a3dc52edae2639ff
danielob19 <dobar19@yahoo.com>
2025-05-21 02:30:41 -0300

    Update __init__.py

--- core/resumen_clinico/__init__.py
+++ core/resumen_clinico/__init__.py
@@ -1,7 +1,2 @@
 # core/resumen_clinico/__init__.py
-from .resumen_clinico import (
-    generar_resumen_clinico_y_estado,
-    generar_resumen_interaccion_5,
-    generar_resumen_interaccion_9,
-    generar_resumen_interaccion_10,
-)
+# Este archivo permite importar el módulo como paquete.
