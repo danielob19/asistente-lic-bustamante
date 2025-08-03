0163a12ebea5a24c66b873137a127ebb9680d84b
danielob19 <dobar19@yahoo.com>
2025-08-03 02:00:36 -0300

    Update asistente.py

--- routes/asistente.py
+++ routes/asistente.py
@@ -1116,7 +1116,7 @@ async def asistente(input_data: UserInput):
                     respuesta_openai=respuesta_ai,
                     sugerencia="",
                     fase_evaluacion="respuesta_peligrosa",
-                    interaccion_id=uuid4(),
+                    interaccion_id=int(time.time()),
                     fecha=datetime.now(),
                     fuente="web",
                     eliminado=False
