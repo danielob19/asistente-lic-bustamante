b5bcdb5c02295d742877e8c69f5147a38d5521cd
danielob19 <dobar19@yahoo.com>
2025-08-09 01:17:12 -0300

    Update resumen_clinico.py

--- core/resumen_clinico.py
+++ core/resumen_clinico.py
@@ -3,7 +3,6 @@ import re
 import random
 import openai  # ✅ Necesario para funciones que usan ChatCompletion
 
-from core.db.sintomas import obtener_coincidencias_sintomas_y_registrar
 from core.db.registro import registrar_respuesta_openai, registrar_emocion, registrar_inferencia
 from cerebro_simulado import predecir_evento_futuro, clasificar_estado_mental
 from core.funciones_asistente import detectar_emociones_negativas
